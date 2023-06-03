4b3f84a9a4bc66bc0141ed8bb79268c387358484
g0urav-hustler <gouravchouhan966@gmail.com>
2023-06-03 18:57:52 +0530

    update app file

--- app.py
+++ app.py
@@ -9,17 +9,14 @@ model_name = "Salesforce/blip-image-captioning-base"
 st.title("Image Caption Generator")
 
 #subtitle
-st.markdown("This application helps you to generate caption on your image")
+st.markdown("This application helps you to generate caption for your image.")
 
-#image uploader
-image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
-
-@st.cache_data(show_spinner="Loading processor")
+@st.cache_data(show_spinner="Loading the app..")
 def load_processor(model_name): 
     processor = BlipProcessor.from_pretrained(model_name) 
     return processor
 
-@st.cache_data(show_spinner="Fetching model ...")  
+@st.cache_data(show_spinner="App Loading..")  
 def load_model(model_name): 
     model = BlipForConditionalGeneration.from_pretrained(model_name) 
     return model
@@ -27,28 +24,30 @@ def load_model(model_name):
 processor = load_processor(model_name)
 model = load_model(model_name)
 
+#image uploader
+image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+
 if image is not None:
 
     input_image = Image.open(image) 
     # st.image(input_image) #display image
 
-    with st.spinner("Creating the captions "):
+    with st.spinner("Creating the captions... "):
 
         raw_image = input_image.convert('RGB')
         inputs = processor(raw_image, return_tensors="pt")
 
         out = model.generate(**inputs)
         result = processor.decode(out[0], skip_special_tokens=True)
-        
 
-
-        result_text = {"Caption": result} 
-
-        st.write(result_text)
+        st.write(result)
         
    
 else:
     st.write("Upload an Image first")
+
+
+st.caption("Made by Gourav Chouhan ")
 
 
 
